3b963dcfb2b76c749734bdb7f059145ca7e4d7b5
kangle0224 <kangle0224@gmail.com>
2018-12-18 22:56:39 +0800

    分页原理

--- app01/views.py
+++ app01/views.py
@@ -9,5 +9,9 @@ for i in range(1,999):
 def index(request):
     num_per_page = 10
     current_page = request.GET.get('p')
+    current_page = int(current_page)
+    start = (current_page-1)*num_per_page
+    end = current_page*num_per_page
+    data = USER_LIST[start: end]
 
-    return render(request, 'index.html', {"user_list": USER_LIST})
+    return render(request, 'index.html', {"user_list": data})
