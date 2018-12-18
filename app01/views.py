1de3d43474c4fd7ab91a25f5ac89fe1939ee632b
kangle0224 <kangle0224@gmail.com>
2018-12-18 23:06:06 +0800

    使用Paginator分页

--- app01/views.py
+++ app01/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, HttpResponse
 
 USER_LIST = []
 for i in range(1,999):
@@ -15,3 +15,17 @@ def index(request):
     data = USER_LIST[start: end]
 
     return render(request, 'index.html', {"user_list": data})
+
+
+def index1(request):
+    current_page = request.GET.get('p')
+    paginator = Paginator(USER_LIST, 10)
+
+    try:
+        posts =paginator.page(current_page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'index1.html', {'posts': posts})
